2ae345de2da089eafdbec893c691440b21ab7f02
supriya <linkin.suppu@gmail.com>
2020-05-16 19:33:50 +0545

    Updates

---
+++ precache-manifest.b08a5ac91ffedea2061911a54e1e4fb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad54bb37e268b67fe94dfe03d3ea5659",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "854147e325060d32cd4f",
+    "url": "/todo-app-react/static/css/main.b89e64e6.chunk.css"
+  },
+  {
+    "revision": "715f48d0e2c31a8c45b6",
+    "url": "/todo-app-react/static/js/2.f57b8740.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.f57b8740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854147e325060d32cd4f",
+    "url": "/todo-app-react/static/js/main.c762cc3e.chunk.js"
+  },
+  {
+    "revision": "a00e1c968102f79c962a",
+    "url": "/todo-app-react/static/js/runtime-main.f7d52782.js"
+  },
+  {
+    "revision": "a42c09576c34774246adcae65bc45535",
+    "url": "/todo-app-react/static/media/checklist-icon.a42c0957.png"
+  },
+  {
+    "revision": "ff2edf2ef36bce7e518449f43ac0c136",
+    "url": "/todo-app-react/static/media/todoLogo.ff2edf2e.svg"
+  }
+]);
